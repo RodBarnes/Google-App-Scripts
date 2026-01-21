c07cdd1dafd2603836d312a60cba1098fdec9f51
AdvApp <advappsw@gmail.com>
2026-01-21 16:03:48 -0700

    Added phone icon

--- Custom-Actions.js
+++ Custom-Actions.js
@@ -1,9 +1,8 @@
 function onOpen() {
   SpreadsheetApp.getUi()
     .createMenu('🌟 Custom Actions')
-    .addItem('Format Phone Number', 'formatPhoneNumber')
+    .addItem('📠 Format Phone Number', 'formatPhoneNumber')
     .addItem('📥 Save Selection as CSV', 'exportSelectionToCSV')
     .addItem('📥 Save Selection as VCF', 'exportSelectionToVCF')
     .addToUi();
 }
-

18f20966632b086bb7167488986e355b6f100c7b
AdvApp <advappsw@gmail.com>
2026-01-21 16:00:56 -0700

    Modified icons

--- Custom-Actions.js
+++ Custom-Actions.js
@@ -1,8 +1,9 @@
 function onOpen() {
   SpreadsheetApp.getUi()
-    .createMenu('📥 Custom Actions')
+    .createMenu('🌟 Custom Actions')
     .addItem('Format Phone Number', 'formatPhoneNumber')
-    .addItem('Save Selection as CSV', 'exportSelectionToCSV')
-    .addItem('Save Selection as VCF', 'exportSelectionToVCF')
+    .addItem('📥 Save Selection as CSV', 'exportSelectionToCSV')
+    .addItem('📥 Save Selection as VCF', 'exportSelectionToVCF')
     .addToUi();
 }
+
